b3225717138823eb8e855f2584d42d6dddf52fbe
Artem Klevakin <klevakin@tochka.com>
2017-06-16 16:29:26 +0500

    ~ db evolution

--- app/AppLoader.scala
+++ app/AppLoader.scala
@@ -1,14 +1,17 @@
 import actors.StatsActor
 import actors.StatsActor.Ping
 import akka.actor.Props
-import controllers.{Assets, Application => ApplicationController}
-import play.api._
-import play.api.routing.Router
-import router.Routes
 import com.softwaremill.macwire._
+import controllers.{Assets, Application => ApplicationController}
 import filters.StatsFilter
+import play.api._
+import play.api.db.evolutions.{DynamicEvolutions, EvolutionsComponents}
+import play.api.db.{DBComponents, HikariCPComponents}
 import play.api.libs.ws.ahc.AhcWSComponents
 import play.api.mvc.Filter
+import play.api.routing.Router
+import router.Routes
+import scalikejdbc.config.DBs
 import services.{SunService, WeatherService}
 
 import scala.concurrent.Future
@@ -30,7 +33,8 @@ class AppApplicationLoader extends ApplicationLoader {
   }
 }
 
-trait AppComponents extends BuiltInComponents with AhcWSComponents {
+trait AppComponents extends BuiltInComponents with AhcWSComponents
+  with EvolutionsComponents with DBComponents with HikariCPComponents {
   lazy val assets: Assets = wire[Assets]
   lazy val prefix: String = "/"
   lazy val router: Router = wire[Routes]
@@ -40,15 +44,20 @@ trait AppComponents extends BuiltInComponents with AhcWSComponents {
   lazy val statsFilter: Filter = wire[StatsFilter]
   override lazy val httpFilters = Seq(statsFilter)
   lazy val statsActor = actorSystem.actorOf(Props(wire[StatsActor]), StatsActor.name)
+  override lazy val dynamicEvolutions: DynamicEvolutions = new DynamicEvolutions
+
 
   val onStart: Unit = {
     Logger.info("The app about to start")
+    applicationEvolutions
+    DBs.setupAll()
     statsActor ! Ping
   }
 
   applicationLifecycle.addStopHook {
     () =>
       Logger.info("The app about to stop")
+      DBs.closeAll()
       Future.successful(Unit)
   }
 }
--- build.sbt
+++ build.sbt
@@ -15,8 +15,14 @@ libraryDependencies ++= Seq(
   ws,
   cache,
   jdbc,
+  evolutions,
+  "org.scalikejdbc" %% "scalikejdbc" % "2.3.5",
+  "org.scalikejdbc" %% "scalikejdbc-config" % "2.3.5",
+  "ch.qos.logback" % "logback-classic" % "1.1.3",
+  "org.postgresql" % "postgresql" % "9.4.1207.jre7",
   "org.scalatestplus.play" %% "scalatestplus-play" % "1.5.0" % "test",
   "org.mockito" % "mockito-core" % "2.0.45-beta",
   "com.softwaremill.macwire" %% "macros" % "2.2.0" % "provided",
-  "com.softwaremill.macwire" %% "util" % "2.2.0"
+  "com.softwaremill.macwire" %% "util" % "2.2.0",
+  "de.svenkubiak" % "jBCrypt" % "0.4.1"
 )
